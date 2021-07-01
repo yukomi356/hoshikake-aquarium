7175bad172f259d04234ecd25c58e5543e395a11
yukomi356 <84891362+yukomi356@users.noreply.github.com>
2021-07-01 16:15:04 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close =document.getElementById('close');
+
+  open.addEventListener('click', ()=> {
+    overlay.classList.add('show');
+  });
+
+  close.addEventListener('click', ()=> {
+    overlay.classList.remove('show');
+  });
+}
